f57eca6fb1c4abaf7e2a5c60c45e1faa00b995f4
DanceMore <dancemore@protonmail.com>
2023-09-11 14:13:04 -0700

    MpdConn for scheduler

--- server/src/main.rs
+++ server/src/main.rs
@@ -29,15 +29,18 @@ fn queue_to_filenames(song_array: Vec<mpd::Song>) -> Vec<String> {
 }
 
 // TODO: move out of main.rs
-fn scheduler_mainbody(
-    mpd_conn: Arc<Mutex<MpdConn>>,
-    song_queue: Arc<Mutex<SongQueue>>,
-    tags_data: Arc<Mutex<TagsData>>,
-) {
+fn scheduler_mainbody(song_queue: Arc<Mutex<SongQueue>>, tags_data: Arc<Mutex<TagsData>>) {
+    // Create a new MpdConn instance for the scheduler
+    let mpd_conn = Arc::new(Mutex::new(
+        MpdConn::new().expect("Failed to create MPD connection"),
+    ));
+
     loop {
         debug!("[-] scheduler firing");
-        // get locks
+        // lock the local connector, should not matter
         let mut locked_mpd_conn = mpd_conn.lock().expect("Failed to lock MPD connection");
+
+        // shared data, these locks matter
         let mut locked_song_queue = song_queue.lock().expect("Failed to lock SongQueue");
         let locked_tags_data = tags_data.lock().expect("Failed to lock TagsData");
 
@@ -49,6 +52,10 @@ fn scheduler_mainbody(
             locked_song_queue.shuffle_and_add(songs);
         }
 
+        // aggressively release locks
+        drop(locked_song_queue);
+        drop(locked_tags_data);
+
         // only do work if the live MPD queue length is less than 2
         // ie: 1 Song now-playing, 1 Song on-deck
         let now_playing_len = locked_mpd_conn
@@ -57,8 +64,9 @@ fn scheduler_mainbody(
             .expect("Failed getting MPD active-queue")
             .len();
 
+        let mut locked_song_queue2 = song_queue.lock().expect("Failed to lock SongQueue");
         if now_playing_len < 2 {
-            if let Some(song) = locked_song_queue.remove() {
+            if let Some(song) = locked_song_queue2.remove() {
                 if let Err(error) = locked_mpd_conn.mpd.push(song.clone()) {
                     // Handle the error here or propagate it up to the caller
                     // In this example, we're printing the error and continuing
@@ -74,10 +82,8 @@ fn scheduler_mainbody(
             let _ = std::io::stdout().flush();
         }
 
-        // release our locks
-        drop(locked_mpd_conn);
-        drop(locked_song_queue);
-        drop(locked_tags_data);
+        // release locks
+        drop(locked_song_queue2);
 
         // Sleep for a while
         thread::sleep(Duration::from_secs(3));
