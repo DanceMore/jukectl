094cec86f7a4e0f96905d0bb365350bf37080713
DanceMore <dancemore@protonmail.com>
2025-02-17 19:21:29 -0800

    try better connection, dns caching

--- server/src/mpd_conn.rs
+++ server/src/mpd_conn.rs
@@ -1,18 +1,20 @@
 use mpd::{error::Result, Client};
 use std::env;
+use std::net::ToSocketAddrs;
 
 pub struct MpdConn {
     pub mpd: Client,
+    address: Vec<std::net::SocketAddr>,
 }
 
 impl MpdConn {
     pub fn new() -> Result<Self> {
         println!("[!] connecting to mpd...");
-        let mpd = MpdConn::connect_mpd()?;
-        Ok(MpdConn { mpd })
+        let (mpd, address) = MpdConn::connect_mpd()?;
+        Ok(MpdConn { mpd, address })
     }
 
-    fn connect_mpd() -> Result<Client> {
+    fn connect_mpd() -> Result<(Client, Vec<std::net::SocketAddr>)> {
         // Get environment variables for MPD configuration
         let host = env::var("MPD_HOST").unwrap_or_else(|_| "localhost".to_string());
         let port: u16 = env::var("MPD_PORT")
@@ -20,25 +22,41 @@ impl MpdConn {
             .parse()
             .expect("Failed to parse MPD_PORT as u16");
 
-        let _password = env::var("MPD_PASS").ok();
+        // Resolve the host and port once
+        let address = (host.as_str(), port).to_socket_addrs()
+            .map_err(|e| mpd::error::Error::Io(e))?
+            .collect::<Vec<std::net::SocketAddr>>();
 
-        // Create an MPD client and connect
-        let mut mpd = Client::connect((host.as_str(), port))?;
+        // Create an MPD client and connect using the resolved address
+        let mut mpd = Client::connect(address[0])?;
 
-        // TODO: upstream does not yet support passwords...
-        //if let Some(pass) = password {
-        //    mpd.password(pass.as_str())?;
-        //}
-
-        // always set to "consume" as part of Jukectl
+        // Set consume to true as part of Jukectl
         mpd.consume(true)?;
 
-        Ok(mpd)
+        Ok((mpd, address))
     }
 
     pub fn reconnect(&mut self) -> Result<()> {
+        println!("[!] Checking connection...");
+        if self.is_connected()? {
+            println!("[+] Connection is alive.");
+            return Ok(());
+        }
+
         println!("[!] Reconnecting to mpd...");
-        self.mpd = MpdConn::connect_mpd()?;
+        let mut new_mpd = Client::connect(self.address[0])?;
+        new_mpd.consume(true)?;
+        self.mpd = new_mpd;
         Ok(())
+    }
+
+    fn is_connected(&mut self) -> Result<bool> {
+        match self.mpd.ping() {
+            Ok(_) => Ok(true),
+            Err(e) => {
+                println!("[!] Connection check failed: {}", e);
+                Ok(false)
+            }
+        }
     }
 }
